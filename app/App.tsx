0d48b279d5057f7d6790f8cf015b1e20b8bed90b
Myles Haynes <hi@myleshaynes.com>
2023-01-03 23:35:02 -0800

    switch to buy me a coffee

--- app/App.tsx
+++ app/App.tsx
@@ -73,41 +73,11 @@ function Footer() {
           src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
           alt="Buy Me A Coffee"
           style={{
-            height: "40px",
-            width: "144px",
+            height: "20px",
+            width: "72px",
           }}
         />
       </a>
-      <form
-        action="https://www.paypal.com/donate"
-        method="post"
-        target="_top"
-        style={{ textAlign: "center", marginTop: 20 }}
-      >
-        <input type="hidden" name="business" value="AH5ZRXXH4H78A" />
-        <input type="hidden" name="no_recurring" value="0" />
-        <input
-          type="hidden"
-          name="item_name"
-          value="If you want to support geodatadownloader.com"
-        />
-        <input type="hidden" name="currency_code" value="USD" />
-        <input
-          type="image"
-          src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif"
-          border="0"
-          name="submit"
-          title="PayPal - The safer, easier way to pay online!"
-          alt="Donate with PayPal button"
-        />
-        <img
-          alt=""
-          border="0"
-          src="https://www.paypal.com/en_US/i/scr/pixel.gif"
-          width="1"
-          height="1"
-        />
-      </form>
     </Box>
   );
 }
