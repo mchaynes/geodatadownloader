b875808f0bbf9752518ca20c1b11bcf50fe09387
Myles Haynes <hi@myleshaynes.com>
2023-06-21 16:56:37 -0700

    removed unused code

--- app/routes/schedule.tsx
+++ app/routes/schedule.tsx
@@ -21,44 +21,6 @@ export default function ScheduleTable() {
   const navigate = useNavigate()
   const location = useLocation()
 
-  const headers = Object.entries({
-    "name": {
-      name: "Job Name",
-      width: 200,
-      flex: 2,
-    },
-    "layer_url": { name: "Layer URL", width: 100, flex: 1 },
-    "format": { name: "Format", width: 75, flex: 1 },
-    "destination": { name: "Destination", width: 75, flex: 1 },
-    "frequency": { name: "Frequency", width: 75 },
-    "column_mapping": { name: "Column Mapping", width: 75, flex: 2 },
-  })
-  const gridColDefs: GridColDef[] = []
-  for (const [id, c] of headers) {
-    gridColDefs.push({
-      field: id,
-      renderCell: (f) => {
-        if (id === "name") {
-          // If we're rendering the job name, render a Link to the details of the job so we can update it
-          return (
-            <Link href={`${f.row["id"]}`} onClick={() => navigate(`${f.row["id"]}`)}>
-              {f.value}
-            </Link>
-          )
-
-        }
-        let val = f.value
-        if (typeof f.value === "object") {
-          val = JSON.stringify(f.value)
-        }
-        return <Typography>{val}</Typography>
-      },
-      headerName: c.name,
-      width: c.width,
-      flex: c.flex,
-    })
-  }
-
   return (
     <Container maxWidth="xl" sx={{ flexDirection: "column" }}>
       <Container maxWidth="xl" style={{ flexDirection: "row" }}>
