b1d0585ac448add9e09e4682572668c9752b884e
Myles Haynes <hi@myleshaynes.com>
2024-01-07 19:07:48 +0800

    Save recently used urls so you can pick em easily later on

--- app/PickLayer.tsx
+++ app/PickLayer.tsx
@@ -3,10 +3,11 @@ import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import { useCallback, useEffect, useState } from "react";
 import * as React from "react";
-import type {} from "@mui/x-data-grid/themeAugmentation";
+import type { } from "@mui/x-data-grid/themeAugmentation";
 import { StatusAlert, useStatusAlert } from "./StatusAlert";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import { setLoadingWhile } from "./loading";
+import { Autocomplete } from "@mui/material";
 
 export type Status = "not_started" | "loading" | "error" | "loaded";
 
@@ -15,10 +16,30 @@ export type PickLayerProps = {
   onLayerLoad: (layer: FeatureLayer) => void;
 };
 
+
+type RecentUrls = {
+  urls: RecentUrl[]
+}
+
+type RecentUrl = {
+  url: string
+}
+
 export function PickLayer({ defaultLayerUrl, onLayerLoad }: PickLayerProps) {
   const [loading, setLoading] = useState(false);
   const [alertProps, setAlertProps] = useStatusAlert("", undefined);
   const [url, setUrl] = useState(defaultLayerUrl);
+
+  const [recentUrls, setRecentUrls] = useState(() => {
+    return JSON.parse(localStorage.getItem("recent_urls") ?? `{"urls": []}`) as RecentUrls
+  })
+
+  const recentUrlsMemod = React.useMemo(() => recentUrls?.urls?.map(u => u.url) ?? [], [recentUrls])
+
+
+  useEffect(() => {
+    localStorage.setItem("recent_urls", JSON.stringify(recentUrls))
+  }, [recentUrls])
 
   const loadLayer = useCallback(
     async (layerUrl: string) => {
@@ -30,6 +51,7 @@ export function PickLayer({ defaultLayerUrl, onLayerLoad }: PickLayerProps) {
           await layer.load();
           setAlertProps("", undefined);
           onLayerLoad(layer);
+          setRecentUrls(urls => ({ urls: [{ url: layerUrl }, ...urls.urls] }))
         } catch (e) {
           const err = e as Error;
           setAlertProps(`${err.message}`, "error");
@@ -72,18 +94,26 @@ export function PickLayer({ defaultLayerUrl, onLayerLoad }: PickLayerProps) {
           gap: "1rem 1rem",
         }}
       >
-        <TextField
-          id="layer-url"
-          required
-          autoComplete="url"
-          fullWidth
-          label="Layer Url"
-          variant="outlined"
-          placeholder="https://gismaps.kingcounty.gov/arcgis/rest/services/Environment/KingCo_SensitiveAreas/MapServer/11"
+        <Autocomplete
+          options={recentUrlsMemod}
           value={url}
-          onChange={(e) => setUrl(e.currentTarget.value)}
-          onKeyPress={handleKeyPress}
+          freeSolo
+
+          onChange={(_, value) => setUrl(value ?? "")}
+          fullWidth
+          renderInput={(params) =>
+            <TextField
+              {...params}
+              id="layer-url"
+              required
+              fullWidth
+              label="Layer Url"
+              placeholder="https://gismaps.kingcounty.gov/arcgis/rest/services/Environment/KingCo_SensitiveAreas/MapServer/11"
+              onKeyPress={handleKeyPress}
+            />
+          }
         />
+
         <Button
           disabled={!url}
           id="load-layer"
