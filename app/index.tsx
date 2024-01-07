4888beecf1c7cd9bb1db41fb078a94873c91d10c
Myles Haynes <hi@myleshaynes.com>
2024-01-07 00:08:45 -0800

    Add sentry (#53)

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,27 @@ import ReactDomClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as Sentry from "@sentry/react";
+
+
+Sentry.init({
+  dsn: "https://43e1102d38f442cb55ebb2110c81c6ef@o4506528837140480.ingest.sentry.io/4506528838909952",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.3, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const rootEl = document.getElementById("root");
 
