91f7a9879867676f076d2d4de6b7a4cb34ae2729
Myles Haynes <hi@myleshaynes.com>
2024-05-16 15:49:25 -0700

    Open url onclick as well as link

--- app/routes/maps/create/index.tsx
+++ app/routes/maps/create/index.tsx
@@ -321,6 +321,7 @@ function LayerDropdownMenu({ layer, boundary }: LayerDropdownMenuProps) {
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
           </svg>
         }
+        onClick={() => window.open(realUrl, "_blank")}
       >
         <Link to={realUrl} target="_blank">
           View
